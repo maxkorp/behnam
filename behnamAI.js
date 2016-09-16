f77b0b796e463418f9d8b9c841a447491d69b63c
Maximiliano Korp <maxkorp@8bytealchemy.com>
2016-09-16 15:57:36 -0700

    initial commit

---
+++ behnamAI.js
@@ -0,0 +1,12 @@
+// behnamAI.js
+while (playerOnField) {
+  if (barg.alive()) {
+    harass(barg);
+    continue;
+  }
+  behnam.dashAttack();
+}
+if(stillAlive) {
+ finishHim(powerAmount);
+}
+// insert the saltiest comment directed at Max below
